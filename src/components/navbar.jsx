9c83300e153f211d4050e3d1b1917384091bbccb
AnggiSatria <anggisatria122@gmail.com>
2022-05-27 00:28:01 +0700

    update13

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,6 @@
 import IMG from "../Assets/Dumbmerch Nav.png";
 import { Link } from "react-router-dom";
+import Font from "../Assets/Font.module.css";
 
 function Navbar(){
     return (
@@ -12,9 +13,9 @@ function Navbar(){
 
                     <div className="right" style={{display : 'flex', flex : '50%', justifyContent : 'flex-end', marginRight : '30px'}}>
                         <ul style={{display : 'flex', listStyleType : 'none', alignItems : 'center'}}>
-                            <Link to="/complain" style={{paddingRight : '20px', textDecoration : 'none'}}><li style={{color : 'white'}}>Complain</li></Link>
-                            <Link to="/profile"style={{paddingRight : '20px', textDecoration : 'none'}}><li style={{color : 'white', textDecoration : 'none'}}>Profile</li></Link>
-                            <Link to="/" style={{textDecoration : 'none'}}><li style={{color : 'white', textDecoration : 'none'}}>Logout</li></Link>
+                            <Link className={Font.Font} to="/complain" style={{paddingRight : '20px', textDecoration : 'none'}}><li style={{color : 'white'}}>Complain</li></Link>
+                            <Link className={Font.Font} to="/profile"style={{paddingRight : '20px', textDecoration : 'none'}}><li style={{color : 'white', textDecoration : 'none'}}>Profile</li></Link>
+                            <Link className={Font.Font} to="/" style={{textDecoration : 'none'}}><li style={{color : 'white', textDecoration : 'none'}}>Logout</li></Link>
                         </ul>
                     </div>
                 </div>
