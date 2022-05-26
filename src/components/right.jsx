98531942c8c7974d26199991c68e5b0d8175415a
AnggiSatria <anggisatria122@gmail.com>
2022-05-26 23:52:46 +0700

    update11

--- src/components/right.jsx
+++ src/components/right.jsx
@@ -4,6 +4,7 @@ import Stack from '@mui/material/Stack';
 import { deepOrange, deepPurple } from '@mui/material/colors';
 import leftImg from "../Assets/left.jpg"
 import rightImg from "../Assets/right.jpg";
+import { Box } from '@mui/material';
 
 const Right = () => {
   return (
@@ -15,8 +16,20 @@ const Right = () => {
           </Stack>
         </div>
 
-        <div className="chactbox" style={{marginLeft : "20px"}}>
+        <div className="chactbox" style={{marginLeft : "20px", display : "flex"}}>
           <img src={leftImg} alt="" style={{width : "15px"}}/>
+          <Box
+      sx={{
+        width: 200,
+        height: 40,
+        backgroundColor: 'rgba(152, 150, 150, 0.46)',
+        borderRadius : '5px',
+        '&:hover': {
+          backgroundColor: 'primary.main',
+          opacity: [0.9, 0.8, 0.7],
+        },
+      }}
+    />
         </div>
       </div>
     </div>
